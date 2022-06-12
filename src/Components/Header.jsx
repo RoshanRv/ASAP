21f8747715b1b511c583df42bf1badaa5f1483c1
Roshan <nkroshankumar@gmail.com>
2022-06-12 17:09:29 +0530

    header updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightToBracket ,faUser,faArrowDown,faBars,faClose,faImages,faBoltLightning,faInfoCircle,faHome} from '@fortawesome/free-solid-svg-icons'
+import { faRightToBracket ,faUser,faArrowDown,faBars,faClose,faImages,faBoltLightning,faCircleInfo,faHome} from '@fortawesome/free-solid-svg-icons'
 
 
 const Header = ({data}) => {
+
+    const [showSideNav,setShowSideNav]=useState(false)
+
   return (
     <header className='absolute slideDown top-0 left-0 p-3 lg:px-12 text-black w-full transition-all duration-500'>
         <nav className="flex text-black/60 justify-between items-center w-full border-b-[2px]  p-3 transition-all duration-500 " style={{borderColor:data.bgColor}} >
@@ -19,18 +22,31 @@ const Header = ({data}) => {
 
             {/*         mobile   */}
             <div className="block md:hidden ">
-                <button className='lg:text-xl text-lg glass  text-black p-2 rounded-xl px-3 transition-all duration-500' style={{color:data.bgColor}} ><FontAwesomeIcon icon={faBars} /></button>
+                <button className='lg:text-xl text-lg glass  text-black p-2 rounded-xl px-3 transition-all duration-500' style={{color:data.bgColor}}
+                onClick={()=>setShowSideNav(true)} ><FontAwesomeIcon icon={faBars} /></button>
             </div>
             {/*         Side Nav         */}
-            <div className='fixed flex  flex-col justify-center items-center w-full h-full z-20 top-0 left-0 transition-all duration-500' style={{backgroundColor:data.color}}>
-                <button style={{color:data.bgColor}} className='text-2xl transition-all duration-500 px-4 absolute top-4 right-4 py-2 glass'><FontAwesomeIcon icon={faClose} /></button>
-                <div className="text-white transition-all duration-500" style={{color:data.bgColor}}  >
-                    <div className="flex items-center gap-x-4 text-2xl">
-                        <FontAwesomeIcon icon={faHome} className='text-xl' />
+            <div className={`fixed flex md:hidden  flex-col justify-center items-center w-full h-full z-20 top-0 ${showSideNav?'right-0':'-right-full'} transition-all duration-500`} style={{backgroundColor:data.color}}>
+                {/*     CLOSE BTN */}
+                <button style={{color:data.bgColor}} className='text-2xl transition-all duration-500 px-4 absolute top-4 right-4 py-2 glass' onClick={()=>setShowSideNav(false)} ><FontAwesomeIcon icon={faClose} /></button>
+                <div className="text-white flex flex-col gap-y-10 transition-all duration-500" style={{color:data.bgColor}}  >
+                    <div className="flex items-center gap-x-4 text-3xl">
+                        <FontAwesomeIcon icon={faHome} className='text-2xl' />
                         <h1 className="">Home</h1>
                     </div>
-                    {/* <h1 className="text-2">Services</h1>
-                    <h1 className="text-2">Gallery</h1> */}
+                    <div className="flex items-center gap-x-4 text-3xl">
+                        <FontAwesomeIcon icon={faBoltLightning} className='text-2xl' />
+                        <h1 className="">Services</h1>
+                    </div>
+                    <div className="flex items-center gap-x-4 text-3xl">
+                        <FontAwesomeIcon icon={faImages} className='text-2xl' />
+                        <h1 className="">Gallery</h1>
+                    </div>
+                    <div className="flex items-center gap-x-4 text-3xl">
+                        <FontAwesomeIcon icon={faCircleInfo} className='text-2xl' />
+                        <h1 className="">About Us</h1>
+                    </div>
+                    
                 </div>
             </div>
         </nav>
